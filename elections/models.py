59794579959d11052ef8197b6b083b356ecd22d5
Amos M. Abdallah <amgenious28@gmail.com>
2026-08-05 02:39:19 +0300

    Create models.py

---
+++ elections/models.py
@@ -0,0 +1,10 @@
+
+# elections/models.py
+from django.db import models
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
